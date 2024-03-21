916f7a2a1c1ab43f5173f99cd6b239a652a503dc
Chris Webb <webbc99@gmail.com>
2024-03-21 22:09:16 +0000

    Adding basic outline of work

---
+++ main.js
@@ -0,0 +1,5 @@
+var movies = ["The Godfather", "The Shawshank Redemption", "The Empire Strikes Back", "Steamboy", "Big Fish", "Akira"]
+
+movies.push("The Green Knight");
+
+console.log(movies[Math.floor(Math.random() * movies.length)]);
