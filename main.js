666e4a5c8532d65d2f2f32e8298ae188fc68a42a
Chris Webb <webbc99@gmail.com>
2024-04-15 14:46:46 +0100

    Adding the remove movie functionality

--- main.js
+++ main.js
@@ -1,40 +1,47 @@
-// var movies = ["The Godfather", "The Shawshank Redemption", "The Empire Strikes Back", "Steamboy", "Big Fish", "Akira"]
-
-// movies.push("The Green Knight");
-
-// console.log(movies[Math.floor(Math.random() * movies.length)]);
-
 var movieArray = []
 var movieName = document.getElementById("movie-name");
 var movieNameSubmitButton = document.getElementById("movie-name-submit-button");
 var movieList = document.getElementById("movie-list");
 var randomizeButton = document.getElementById("randomize-button");
 var movieResult = document.getElementById("movie-result");
+var movies = document.getElementsByClassName("movie");
 
 // Add initial list items to the array
 for (i = 0; i < movieList.children.length; i++) {
     movieArray.push(movieList.children[i].innerText);
 }
 
+// Add the close button to each movie
+for (i = 0; i < movies.length; i++) {
+    var span = document.createElement("span");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    movies[i].appendChild(span);
+}
 
+// When you click a close button, remove the parent movie from the list
+var closeButtons = document.getElementsByClassName("close");
+for (i = 0; i < closeButtons.length; i++) {
+    closeButtons[i].addEventListener("click", function() {
+        this.parentElement.style.display = "none";
+        // Remove the movie from the array
+        var index = movieArray.indexOf(this.parentElement.innerText);
+        movieArray.splice(index, 1);
+    })
+}
+
+// Add the functionality to select a random movie
 randomizeButton.addEventListener("click", getRandomMovie);
 
 function getRandomMovie() {
     for (i = 0; i < movieList.children.length; i++) {
         movieList.children[i].classList.remove("selected-movie");
     }
-
-    // document.querySelector(".selected-movie").classList.remove("selected-movie");
-
     movieList.children[Math.floor(Math.random() * movieList.children.length)].classList.add("selected-movie");
-
-    // var randomMovie = movieArray[Math.floor(Math.random() * movieArray.length)]
-    // movieResult.innerHTML = randomMovie;
-
 }
 
-
-
+// Allow users to add new movies either by clicking Submit or hitting Enter
 movieNameSubmitButton.addEventListener("click", newMovie);
 
 movieName.addEventListener("keydown", (e) => {
@@ -45,6 +52,7 @@ movieName.addEventListener("keydown", (e) => {
 
 function newMovie() {
     var li = document.createElement("li");
+    li.className = "movie";
     var inputValue = movieName.value;
     var text = document.createTextNode(inputValue);
     li.appendChild(text);
@@ -58,5 +66,22 @@ function newMovie() {
         movieArray.push(inputValue);
     }
     
+    var span = document.createElement("span");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    var closeButtons = document.getElementsByClassName("close");
+    for (i = 0; i < closeButtons.length; i++) {
+        closeButtons[i].addEventListener("click", function (){
+            this.parentElement.style.display = "none";
+            // Remove the movie from the array
+            var index = movieArray.indexOf(this.parentElement.innerText);
+            movieArray.splice(index, 1);
+        })
+    }
+    
+
     movieName.value = "";
 }
