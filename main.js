bd9d2fb815e6700922a60be1158b6adc2ef883f6
Chris Webb <webbc99@gmail.com>
2024-04-15 22:15:50 +0100

    fixing randomize and moving to init function

--- main.js
+++ main.js
@@ -5,31 +5,47 @@ var movieList = document.getElementById("movie-list");
 var randomizeButton = document.getElementById("randomize-button");
 var movieResult = document.getElementById("movie-result");
 var movies = document.getElementsByClassName("movie");
+var closeButtons = document.getElementsByClassName("close");
+
+init();
+
+function init() {
+    addExistingMoviesToArray()
+    addCloseButtonsToExistingMovies();
+    addRemovalFunctionalityToExistingCloseButtons();
+}
 
 // Add initial list items to the array
-for (i = 0; i < movieList.children.length; i++) {
-    movieArray.push(movieList.children[i].innerText);
+function addExistingMoviesToArray() {
+    for (i = 0; i < movieList.children.length; i++) {
+        movieArray.push(movieList.children[i].innerText);
+    }
 }
 
-// Add the close button to each movie
-for (i = 0; i < movies.length; i++) {
-    var span = document.createElement("span");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    movies[i].appendChild(span);
+// Add the close button to the movie
+function addCloseButtonsToExistingMovies() {
+    for (i = 0; i < movies.length; i++) {
+        var span = document.createElement("span");
+        var text = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(text);
+        movies[i].appendChild(span);
+    }
 }
 
-// When you click a close button, remove the parent movie from the list
-var closeButtons = document.getElementsByClassName("close");
-for (i = 0; i < closeButtons.length; i++) {
-    closeButtons[i].addEventListener("click", function() {
-        this.parentElement.remove();
-        // Remove the movie from the array
-        var index = movieArray.indexOf(this.parentElement.innerText);
-        movieArray.splice(index, 1);
-    })
+// Get the movie name from the parent li, find it in the array and remove it
+function addRemovalFunctionalityToExistingCloseButtons() {
+    for (i = 0; i < closeButtons.length; i++) {
+        closeButtons[i].addEventListener("click", function() {
+            this.parentElement.remove();
+            var oldText = this.parentElement.innerText;
+            var newText = oldText.substring(0, oldText.length - 1);
+            var index = movieArray.indexOf(newText);
+            movieArray.splice(index, 1);
+        })
+    }
 }
+
 
 // Add the functionality to select a random movie
 randomizeButton.addEventListener("click", getRandomMovie);
@@ -38,12 +54,11 @@ function getRandomMovie() {
     for (i = 0; i < movieList.children.length; i++) {
         movieList.children[i].classList.remove("selected-movie");
     }
-    movieList.children[Math.floor(Math.random() * movieArray.length)].classList.add("selected-movie");
+    movieList.children[Math.floor(Math.random() * movieList.children.length)].classList.add("selected-movie");
 }
 
 // Allow users to add new movies either by clicking Submit or hitting Enter
 movieNameSubmitButton.addEventListener("click", newMovie);
-
 movieName.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
         newMovie()
@@ -65,23 +80,21 @@ function newMovie() {
         movieList.appendChild(li);
         movieArray.push(inputValue);
     }
-    
+
     var span = document.createElement("span");
-    var txt = document.createTextNode("\u00D7");
+    var text = document.createTextNode("\u00D7");
     span.className = "close";
-    span.appendChild(txt);
+    span.appendChild(text);
     li.appendChild(span);
 
-    var closeButtons = document.getElementsByClassName("close");
-    for (i = 0; i < closeButtons.length; i++) {
-        closeButtons[i].addEventListener("click", function (){
-            this.parentElement.remove();
-            // Remove the movie from the array
-            var index = movieArray.indexOf(this.parentElement.innerText);
-            movieArray.splice(index, 1);
-        })
-    }
+    span.addEventListener("click", function() {
+        this.parentElement.remove();
+        var oldText = this.parentElement.innerText;
+        var newText = oldText.substring(0, oldText.length - 1);
+        var index = movieArray.indexOf(newText);
+        movieArray.splice(index, 1);
+        removeElementFromArray(movieArray, newText);
+    })
     
-
     movieName.value = "";
 }
